42280ae6e19f0b6f51541ef956ad9af02a04b4db
ravitejasimhadri683 <ravitejasimhadri683@gmail.com>
2025-03-22 17:07:17 +0530

    Auto Push

---
+++ 06-if_else.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+service=$1
+if [service == start]; then
+    echo "service is started"
+    elif [service == restart]; then
+    echo "service is restarted"
+    else ["service" == "stop"]; then
+    echo "service is stopped"
+  
+fi
