eaabc1864d8ac6713c45ad401f6677c8a2c8dcdc
ravitejasimhadri683 <ravitejasimhadri683@gmail.com>
2025-03-22 14:25:43 +0530

    Auto Push

--- 04-specialVariables.sh
+++ 04-specialVariables.sh
@@ -12,5 +12,5 @@ echo "printing working place as agument: $place"
 echo "$0"  #print script name
 echo "$?"  #print last execution stattus, if it is 0 means success, others means fail
 echo "$#"  #number of arugument passed
-echo "$@"  #Number of arugment used 
+echo "$@"  #print arugment values which you passed
 
