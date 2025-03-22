90063f26f203e0c7e7c1a8423653fe7e729a5439
ravitejasimhadri683 <ravitejasimhadri683@gmail.com>
2025-03-22 12:20:56 +0530

    Auto Push

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 a=10
 echo "printing a value $a"
-echo "printing a value $b"
+echo "printing a value $b"  # it will print null/empty value
 echo "printing a value ${a}"
-echo 'printing a value $a'
+echo 'printing a value $a'  # single quote will remove special power of Variable 
