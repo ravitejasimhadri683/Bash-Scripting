d2810582927875f7a4c3e8cadfd4bd7e3f8f2c81
ravitejasimhadri683 <ravitejasimhadri683@gmail.com>
2025-03-22 17:19:11 +0530

    Auto Push

--- 06-if_else.sh
+++ 06-if_else.sh
@@ -2,5 +2,9 @@
 ACTION=$1
 if [ "$ACTION" == "start" ]; then
     echo "service is started"
+elif [ "$ACTION" == "restart" ]; then
+    echo "service is restarted"
+else
+    echo "service is stopped"
 fi
 echo "script completed!"
