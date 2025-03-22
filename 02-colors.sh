da9b002e4ffba7ee3677adba051b9d256834c6ee
ravitejasimhadri683 <ravitejasimhadri683@gmail.com>
2025-03-22 11:49:33 +0530

    Auto Push

--- 02-colors.sh
+++ 02-colors.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "Printing text in color format"
-echo -e "\e[31m Printing yellow color \e[0m "
+echo -e "\e[31m Printing Red color \e[0m "
+echo -e "\e[32m Printing another coloe \e[0m"
+echo -e "\e[33m Printing another color \e[om"
