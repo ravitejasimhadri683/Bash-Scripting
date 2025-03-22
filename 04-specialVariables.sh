6a6546e54f53c8fabaa3290458b8d7498a84d31b
ravitejasimhadri683 <ravitejasimhadri683@gmail.com>
2025-03-22 14:19:58 +0530

    Auto Push

--- 04-specialVariables.sh
+++ 04-specialVariables.sh
@@ -2,8 +2,12 @@
 echo "printing special variables"
 name="Ravi"
 job="IT"
+team=$1
+place=$2
 echo "printing variable 1 + $name"
 echo "printing variable 2 + $job"
+echo "printing arugument value 1 + $team"
+echo "printing arugument value 2 + $place"
 echo "$?"
 echo "$#"
 
